cbdc67d1ddb95a53dcdd6279c172c67c77a33f19
Mamednoor <mamed.gohabur@gmail.com>
2020-07-20 03:22:23 +0200

    initialize project

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,22 @@
+// @flow
+
+const express = require("express")
+const app = express()
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+app.get("/", (request, response) => {
+  response.send("Bienvenue sur Express");
+});
+
+app.listen(process.env.PORT || 8080, (err) => {
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+  console.log("Server is up");
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
